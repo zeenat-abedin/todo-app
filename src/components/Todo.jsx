307e1bfd7db0bf468e55413a3efa7b40d33b3915
zeenat <zeenu.23@gmail.com>
2024-03-30 22:26:50 +0530

    "Add task component and initial tasks state to Todo component"

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,15 +1,35 @@
-import React from 'react'
+import  { useState } from 'react'
+import Task from './Task'
 
 function Todo() {
+  const [tasks, setTasks] = useState([
+    {
+      title: "Buying a pizza",
+      completed: false
+    },
+    {
+      title: "Do your workout",
+      completed: true
+    },
+    {
+      title: "Hangout with friends",
+      completed: true
+    }
+  ])
+
     return (
     <div className='todo-container'>
     <div className='header'>
       TODO ITEMS
     </div>
-            <div className="tasks"></div>
-            <div className="create-task">
-                <input type="text" placeholder="Enter Task Here..." id="newTask"/>
-            </div>
+        <div className="tasks">
+          {tasks.map((task, index) => (
+            <Task/>
+          ))}
+        </div>
+        <div className="create-task">
+            <input type="text" placeholder="Enter Task Here..." id="newTask"/>
+        </div>
     </div>
   )
 }
