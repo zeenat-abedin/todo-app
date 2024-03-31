1f71641654ace0995811cd90834843eba257ecb2
zeenat <zeenu.23@gmail.com>
2024-03-31 12:36:08 +0530

    "Added 'completed' callback to Task component to toggle task completion"

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 function Task({task,completed}) {
   return (
-      <div className='task' style={{ textDecoration: completed ? "line-through" : "" }}>
+    <div className='task' style={{ textDecoration: completed ? "line-through" : "" }}>
       {task.title}
+      <button onClick={() => completed(task)}>{completed ? "Mark Undone" : "Mark Done"}</button>
     </div>
   )
 }
